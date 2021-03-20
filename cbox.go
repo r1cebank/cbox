ee1e413282baa2076313066a1e36724bd1bb4115
r1cebank <siyuangao@protonmail.com>
2021-03-19 17:48:59 -0700

    Adding stuff

--- cbox.go
+++ cbox.go
@@ -9,21 +9,7 @@ import (
 )
 
 func main() {
-	// Get the current user os, if supported, continue
-	prompt := promptui.Select{
-		Label: "Select Day",
-		Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-			"Saturday", "Sunday"},
-	}
 
-	_, result, err := prompt.Run()
-
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	os.OSSupported()
-
+    os.GetOperatingSystem()
 	fmt.Printf("You choose %q\n", result)
 }
